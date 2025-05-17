450683e36ca858109198c95bfce0eb05efa71bf1
Louis-rollet <louis.rollet@epitech.eu>
2025-05-17 09:18:12 +0000

    [BACKEND] [FIX] updated Swagger setup route from 'api' to 'swagger'

--- src/swagger.ts
+++ src/swagger.ts
@@ -18,7 +18,7 @@ export function setupSwagger(app: INestApplication) {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document, {
+  SwaggerModule.setup('swagger', app, document, {
     swaggerOptions: {
       persistAuthorization: true,
     },
