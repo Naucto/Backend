b165f3198afba9da773811ae5c569e2cc1a76d4b
Alexis Belmonte <alexbelm48@gmail.com>
2026-03-01 12:29:10 -0500

    [JEST] [FIX] Integration from previous merge

--- src/routes/user/user.controller.spec.ts
+++ src/routes/user/user.controller.spec.ts
@@ -2,6 +2,9 @@ import { Test, TestingModule } from "@nestjs/testing";
 import { UserController } from "./user.controller";
 import { UserService } from "./user.service";
 import { PrismaService } from "@ourPrisma/prisma.service";
+import { S3Service } from "@s3/s3.service";
+import { CloudfrontService } from "@s3/cloudfront.service";
+import { ConfigService } from "@nestjs/config";
 
 describe("UserController", () => {
   let controller: UserController;
@@ -15,7 +18,38 @@ describe("UserController", () => {
           provide: PrismaService,
           useValue: {
             $connect: jest.fn(),
-            $disconnect: jest.fn()
+            $disconnect: jest.fn(),
+            user: {
+              findUnique: jest.fn(),
+              findMany: jest.fn(),
+              update: jest.fn(),
+              delete: jest.fn(),
+              count: jest.fn()
+            }
+          }
+        },
+        {
+          provide: S3Service,
+          useValue: {
+            uploadFile: jest.fn(),
+            fileExists: jest.fn(),
+            downloadFile: jest.fn(),
+            deleteFile: jest.fn()
+          }
+        },
+        {
+          provide: CloudfrontService,
+          useValue: {
+            getCDNUrl: jest.fn(),
+            createSignedCookies: jest.fn(),
+            getCookieDomain: jest.fn(),
+            generateSignedUrl: jest.fn()
+          }
+        },
+        {
+          provide: ConfigService,
+          useValue: {
+            get: jest.fn()
           }
         }
       ]
