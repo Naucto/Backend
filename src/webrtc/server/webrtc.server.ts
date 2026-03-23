4ca941cb762b4258444fcb8ebe5cf642b58c4b39
Alexis Belmonte <alexbelm48@gmail.com>
2026-03-22 20:59:40 -0400

    [WEBRTC] [UPDATE] Add auth/upgrade event handling

--- src/webrtc/server/webrtc.server.ts
+++ src/webrtc/server/webrtc.server.ts
@@ -3,8 +3,9 @@
 import { WebRTCServerDecoratorError, WebRTCServerRuntimeError } from "@webrtc/server/webrtc.server.error";
 
 import { availableParallelism } from "os";
-import { IncomingMessage } from "http";
 
+import { Socket as TCPSocket } from "net";
+import { Server as HTTPServer, createServer as createHttpServer, IncomingMessage } from "http";
 import { WebSocket, WebSocketServer } from "ws";
 import { Logger } from "@nestjs/common";
 
@@ -12,28 +13,54 @@ interface WebRTCSocketLikeObject {
   on(event: string, listener: (...args: unknown[]) => void): void;
 };
 
-type WebRTCEventHandler = (...args: unknown[]) => void;
-type WebRTCEventHandlerMap = Map<string, WebRTCEventHandler>;
+// https://developer.mozilla.org/en-US/docs/Web/API/WebSocket/readyState
+export enum WebRTCClientReadyState {
+  CONNECTING = 0,
+  OPEN       = 1,
+  CLOSING    = 2,
+  CLOSED     = 3
+};
+
+type WebRTCClientSocketData = {
+  readyState: WebRTCClientReadyState;
+  remoteAddress: string | undefined;
+};
+
+export type WebRTCClientSocket<TExtraData = object> =
+  Omit<WebSocket, keyof WebRTCClientSocketData> & TExtraData & WebRTCClientSocketData;
+export type WebRTCServerSocket<TExtraData = object> =
+  WebSocketServer & TExtraData;
+
+type WebRTCEventHandler        = (...args: unknown[]) => void;
+type WebRTCEventHandlerMap     = Map<string, WebRTCEventHandler>;
+
+type WebRTCAuthEventHandler    = (
+  httpRequest: IncomingMessage, httpClientSocket: TCPSocket, head: Buffer
+) => boolean;
+type WebRTCAuthEventHandlerSet = Array<WebRTCAuthEventHandler>;
 
 type WebRTCEventKind = "server" | "client";
 
-function WebRTCBaseEvent(eventLevel: WebRTCEventKind, eventName: string): MethodDecorator
+function WebRTCBaseEvent(
+  eventLevel: WebRTCEventKind,
+  eventName: string): MethodDecorator
 {
   const decoratorWrapper: MethodDecorator = (
     target: unknown,
-    _: string | symbol,
+    _key: string | symbol,
     descriptor: PropertyDescriptor
   ) => {
     if (!(target instanceof WebRTCServer)) {
       throw new WebRTCServerDecoratorError(
-        `The @WebRTC${eventLevel[0]!.toUpperCase() + eventLevel.slice(1)}Event decorator can only be applied to methods of the WebRTCServer class.`
+        `The @WebRTC${eventLevel[0]!.toUpperCase() + eventLevel.slice(1)}Event ` +
+        "decorator can only be applied to methods of the WebRTCServer and " +
+        "derived classes"
       ); 
     }
 
     const targetAsFields = target as Record<keyof WebRTCServer, unknown>;
-    const eventMapKey = `_${eventLevel}EventHandlers` as keyof WebRTCServer;
-    const eventMap: WebRTCEventHandlerMap =
-      targetAsFields[eventMapKey] as WebRTCEventHandlerMap;
+    const eventMapKey    = `_${eventLevel}EventHandlers` as keyof WebRTCServer;
+    const eventMap       = targetAsFields[eventMapKey] as WebRTCEventHandlerMap;
 
     if (!eventMap) {
       // This shouldn't happen
@@ -62,8 +89,38 @@ export function WebRTCClientEvent(eventName: string): MethodDecorator
   return WebRTCBaseEvent("client", eventName);
 }
 
+export function WebRTCServerAuthEvent(): MethodDecorator
+{
+  const decoratorWrapper: MethodDecorator = (
+    target: unknown,
+    _key: string | symbol,
+    descriptor: PropertyDescriptor
+  ) => {
+    if (!(target instanceof WebRTCServer)) {
+      throw new WebRTCServerDecoratorError(
+        "The @WebRTCServerAuthEvent decorator can only be applied to methods " +
+        "of the WebRTCServer and derived class"
+      ); 
+    }
+
+    const targetAsFields = target as Record<keyof WebRTCServer, unknown>;
+    const eventMapKey    = "_authEventHandlers" as keyof WebRTCServer;
+    const eventMap       = targetAsFields[eventMapKey] as WebRTCAuthEventHandlerSet;
+
+    if (!eventMap) {
+      // This shouldn't happen
+      throw new WebRTCServerDecoratorError("No auth event handler map found");
+    } else if (eventMap.has(descriptor.value)) {
+      throw new WebRTCServerDecoratorError("Duplicate auth event handler");
+    }
+  };
+
+  return decoratorWrapper;
+}
+
 export class WebRTCServerOptions {
-  compressedThreshold: number = 256;
+  compressed: boolean = true;
+  compressionThreshold: number = 256;
 };
 
 // Bare-bones implementation of a generic WebRTC server.
@@ -71,11 +128,13 @@ export class WebRTCServerOptions {
 export class WebRTCServer<OptsT extends WebRTCServerOptions = WebRTCServerOptions> {
   private readonly _logger: Logger;
 
-  private readonly _wss: WebSocketServer;
+  private readonly _httpServer: HTTPServer;
+  private readonly _wsServer: WebSocketServer;
   private readonly _extraOpts: OptsT;
 
-  private readonly _serverEventHandlers: WebRTCEventHandlerMap = new Map();
-  private readonly _clientEventHandlers: WebRTCEventHandlerMap = new Map();
+  private readonly _authEventHandlers:   WebRTCAuthEventHandlerSet = [];
+  private readonly _serverEventHandlers: WebRTCEventHandlerMap     = new Map();
+  private readonly _clientEventHandlers: WebRTCEventHandlerMap     = new Map();
 
   constructor(
     whatFor: string,
@@ -84,9 +143,10 @@ export class WebRTCServer<OptsT extends WebRTCServerOptions = WebRTCServerOption
   ) {
     this._logger = new Logger(`${this.constructor.name} (${whatFor})`);
 
-    this._wss = new WebSocketServer({
-      port,
-      perMessageDeflate: {
+    this._httpServer = createHttpServer();
+    this._wsServer = new WebSocketServer({
+      noServer: true,
+      perMessageDeflate: extraOpts.compressed ? {
         zlibDeflateOptions: {
           // Use page-sized chunks for compression
           chunkSize: 4096,
@@ -102,25 +162,31 @@ export class WebRTCServer<OptsT extends WebRTCServerOptions = WebRTCServerOption
         // Use all cores minus 2 so that the server can still respond to requests
         concurrencyLimit: availableParallelism() - 2,
         // Don't compress if smaller than the given amount of bytes
-        threshold: extraOpts.compressedThreshold
-      }
+        threshold: extraOpts.compressionThreshold
+      } : {}
     });
 
     this._extraOpts = extraOpts;
 
-    this.applyEventHandlers(this._wss);
+    this.applyEventHandlers(this._wsServer);
+
+    this._httpServer.listen(port);
   }
+
+  // --------------------------------------------------------------------------
 
   public get logger(): Logger {
     return this._logger;
   }
 
-  protected get wss(): WebSocketServer {
-    return this._wss;
-  }
-
   protected get extraOpts(): OptsT {
     return this._extraOpts;
+  }
+
+  // --------------------------------------------------------------------------
+
+  protected wss<T extends WebSocketServer>(): T {
+    return this._wsServer as T;
   }
 
   // --------------------------------------------------------------------------
@@ -136,7 +202,7 @@ export class WebRTCServer<OptsT extends WebRTCServerOptions = WebRTCServerOption
     eventHandlers.forEach((handler, eventName) => {
       socket.on(eventName, (...args) => {
         try {
-          handler.apply(this, args);
+          handler.apply(this, [ specializedSocket, ...args ]);
         } catch (error) {
           let message;
 
@@ -146,18 +212,66 @@ export class WebRTCServer<OptsT extends WebRTCServerOptions = WebRTCServerOption
             message = String(error);
           }
 
-          this._logger.error(
-            `Failed to hook "${eventName}": ${message}`
+          this._logger.error(`Failed to hook "${eventName}": ${message}`);
+
+          throw new WebRTCServerRuntimeError(
+            `Failed to hook "${eventName}" event handler`
           );
-          throw new WebRTCServerRuntimeError("Failed to hook `${eventName}` event handler.");
         }
       });
     });
   }
 
   @WebRTCServerEvent("upgrade")
-  protected onUpgrade(_request: IncomingMessage, socket: WebSocket): void
+  protected _internal_base_onUpgrade(
+    _serverSocket: WebSocketServer,
+    request: IncomingMessage,
+    httpClientSocket: TCPSocket,
+    head: Buffer 
+  ): void {
+    this._wsServer.handleUpgrade(request, httpClientSocket, head,
+      (clientSocket: WebSocket, ...args: unknown[]) => {
+        let authHandlerI: number = -1;
+
+        try {
+          this._authEventHandlers.forEach((handler, i) => {
+            authHandlerI = i;
+
+            if (!handler(request, httpClientSocket, head)) {
+              this.logger.verbose(
+                `Handler ${authHandlerI} returned false, denying access`
+              );
+
+              httpClientSocket.destroy();
+              return;
+            }
+          });
+        } catch (err) {
+          this.logger.verbose(
+            `Exception while executing auth handler #${authHandlerI}: ` +
+            `${err}`
+          );
+
+          httpClientSocket.destroy();
+          return;
+        }
+
+        this.applyEventHandlers(clientSocket);
+
+        this._wsServer.emit("connection", clientSocket, request, ...args);
+      }
+    );
+  }
+
+  @WebRTCServerEvent("connection")
+  protected _internal_base_onConnection(
+    _serverSocket: WebSocketServer,
+    rawClientSocket: WebSocket,
+    httpRequest: IncomingMessage
+  ): void
   {
-    this.applyEventHandlers(socket);
+    const clientSocket = rawClientSocket as WebRTCClientSocket;
+
+    clientSocket.remoteAddress = httpRequest.socket.remoteAddress;
   }
 };
