9ef4d932a544d91534433b493e1fb5c4b5dcbefc
Priax <priax.xairp@gmail.com>
2026-05-03 10:55:51 +0200

    [BACKEND] [Fix] tests not passing

--- jest.config.ts
+++ jest.config.ts
@@ -22,6 +22,7 @@ const config: Config = {
       },
     ],
   },
+  transformIgnorePatterns: ["/node_modules/(?!(jose|jwks-rsa)/)"],
   collectCoverage: true,
   collectCoverageFrom: ["**/*.(t|j)s"],
   coverageDirectory: "../coverage",
